475e1d5274ca21a13016081969a4a33c9444c9ba
Rahul Patidar <rahulpatidar.rails@gmail.com>
2018-03-20 19:35:20 +0530

    initial commit

---
+++ src/containers/welcome/welcome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+class Welcome {
+  constructor() {
+    super(props);
+    this.state = {};
+  }
+}
